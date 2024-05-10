86140a019f9b5d0f6aadf49be1a9b5509fd6bb55
Tau Gift <taugift@gmail.com>
2024-05-10 14:28:28 +0200

    First Change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    Scanner input = new Scanner(System.in);
+    System.out.println("your name");
+    String name = input.next();
+    System.out.println("Welcome "+ name);
+    System.out.println("Hello world!");
+
+}
 }
